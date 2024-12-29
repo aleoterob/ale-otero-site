41082d67419bf660a8ecf0ce384da61219d01cef
Alejandro Otero <aleoterob@gmail.com>
2024-12-29 19:36:04 -0300

    Update hero home animation

--- src/app/ui/hero-sections/hero-home.tsx
+++ src/app/ui/hero-sections/hero-home.tsx
@@ -1,74 +1,52 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import gsap from "gsap";
 
 const HeroHome = () => {
-  const boxRef = useRef(null);
-
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const lineOne = document.querySelector(".one");
-      const lineTwo = document.querySelector(".two");
-      const siteWord = document.querySelector(".site");
-      const allLinks = gsap.utils.toArray("p a");
+      const alejandro = document.querySelector(".alejandro");
+      const otero = document.querySelector(".otero");
+      const portfolio = document.querySelector(".portfolio");
+      const site = document.querySelector(".site");
 
-      console.log("lineOne:", lineOne);
-      console.log("lineTwo:", lineTwo);
-      console.log("siteWord:", siteWord);
-      console.log("allLinks:", allLinks);
+      console.log("alejandro:", alejandro);
+      console.log("otero:", otero);
+      console.log("portfolio:", portfolio);
+      console.log("site:", site);
 
       const tl = gsap.timeline({
         defaults: { duration: 1, ease: "power2.out" },
       });
 
       // Configuración inicial
-      gsap.set([lineOne], { xPercent: -101, autoAlpha: 0 });
-      gsap.set(lineTwo, { xPercent: 101, autoAlpha: 0 });
-      gsap.set(siteWord, { xPercent: 101, autoAlpha: 0 });
-      gsap.set(allLinks, {
-        y: -100,
-        autoAlpha: 0,
-        scale: 1.5,
-        rotationX: 45,
-      });
+      gsap.set(alejandro, { xPercent: -101, autoAlpha: 0 });
+      gsap.set(otero, { xPercent: 101, autoAlpha: 0 });
+      gsap.set(portfolio, { xPercent: 101, autoAlpha: 0 });
+      gsap.set(site, { xPercent: 101, autoAlpha: 0 });
 
       // Animaciones
-      tl.to(lineOne, { xPercent: 0, autoAlpha: 1 })
-        .to(lineTwo, { xPercent: 0, autoAlpha: 1 }, "-=0.5")
-        .to(siteWord, { xPercent: 0, autoAlpha: 1 }, "-=0.3")
-        .to(allLinks, {
-          y: 0,
-          autoAlpha: 1,
-          scale: 1,
-          stagger: 0.4,
-          rotationX: 0,
-        });
+      tl.to(alejandro, { xPercent: 0, autoAlpha: 1 })
+        .to(otero, { xPercent: 0, autoAlpha: 1 }, "-=0.5")
+        .to(portfolio, { xPercent: 0, autoAlpha: 1 }, "-=0.3")
+        .to(site, { xPercent: 0, autoAlpha: 1 }, "-=0.3");
     } else {
       console.error("One or more elements are not found in the DOM.");
     }
   }, []);
 
   return (
-    <div
-      className="text-right  text-white font-montserrat font-light text-[110px] pt-[10vh] h-screen uppercase overflow-hidden [perspective:1000px]"
-      ref={boxRef}
-    >
-      <p className="block whitespace-nowrap m-0 one" ref={boxRef}>
-        <span ref={boxRef}>alejandro</span>
-        <a
-          href="#about"
-          className="text-white no-underline inline-block font-bold"
-        >
+    <div className="text-right  text-white font-montserrat font-light text-[110px] pt-[10vh] h-screen uppercase overflow-hidden [perspective:1000px]">
+      <p className="block whitespace-nowrap m-0 alejandro">
+        <span>alejandro</span>
+        <span className="text-white no-underline inline-block font-bold otero">
           otero
-        </a>
+        </span>
       </p>
-      <p className="block whitespace-nowrap m-0 two" ref={boxRef}>
-        <span ref={boxRef}>portfolio</span>
-        <span
-          className="text-[#47cca5] no-underline inline-block font-bold site"
-          ref={boxRef}
-        >
+      <p className="block whitespace-nowrap m-0 two portfolio">
+        <span>portfolio</span>
+        <span className="text-[#47cca5] no-underline inline-block font-bold site">
           site
         </span>
       </p>
