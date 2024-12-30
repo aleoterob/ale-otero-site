f856153eb9408b5821c35f1df36ecd09a66fb7b8
Alejandro Otero <aleoterob@gmail.com>
2024-12-30 19:16:04 -0300

    Updates slide menu

--- src/app/ui/navigation/slide-menu.tsx
+++ src/app/ui/navigation/slide-menu.tsx
@@ -8,13 +8,11 @@ export default function SlideMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    // Selecciona el elemento con la clase "menu-icon" en el Header
     const menuIcon = document.querySelector(".menu-icon");
     if (menuIcon) {
       menuIcon.addEventListener("click", () => setIsOpen(true));
     }
 
-    // Limpia el evento al desmontar
     return () => {
       if (menuIcon) {
         menuIcon.removeEventListener("click", () => setIsOpen(true));
@@ -34,12 +32,12 @@ export default function SlideMenu() {
     <>
       {/* Slide Menu */}
       <div
-        className={`fixed top-0 right-0 h-full bg-verdeAle transition-transform duration-500 ease-in-out w-96 ${
+        className={`fixed top-0 right-0 h-full bg-background transition-transform duration-1000 ease-in-out w-96 ${
           isOpen ? "translate-x-0" : "translate-x-full"
-        } w-64 shadow-lg z-50`}
+        } z-50`}
       >
         {/* Contenido del Slide Menu */}
-        <div className="px-8 py-4 flex flex-col h-full z-50">
+        <div className="px-8 py-4 flex flex-col h-full menu-container bg-negroSlideMenu shadow-2xl shadow-negroPuro">
           {/* Botón de cerrar */}
           <div className="flex justify-end">
             <Image
@@ -54,17 +52,31 @@ export default function SlideMenu() {
 
           <Link
             href="/"
-            className="p-1 text-4xl text-white hover:text-black"
+            className="p-1 text-3xl text-foreground hover:text-grisHover"
             onClick={handleLinkClick}
           >
-            Home
+            HOME
+          </Link>
+          <Link
+            href="/#about-me-id"
+            className="p-1 text-3xl text-foreground hover:text-grisHover"
+            onClick={handleLinkClick}
+          >
+            ABOUT ME
+          </Link>
+          <Link
+            href="/"
+            className="p-1 text-3xl text-foreground hover:text-grisHover"
+            onClick={handleLinkClick}
+          >
+            PORTFOLIO
           </Link>
           <Link
             href="/contact"
-            className="p-1 text-4xl text-white hover:text-black"
+            className="p-1 text-3xl text-foreground hover:text-grisHover"
             onClick={handleLinkClick}
           >
-            Contact
+            CONTACT
           </Link>
         </div>
       </div>
