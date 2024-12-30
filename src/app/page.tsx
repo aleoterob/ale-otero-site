54ce31fd8ff7819be2430aad69de551712b607a4
Alejandro Otero <aleoterob@gmail.com>
2024-12-30 16:06:19 -0300

    Adds footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,90 +10,97 @@ import DesignSkillsChart from "./ui/charts/design-skills-chart";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pb-8 w-1490">
-      <HeroHome />
-      <div className="flex flex-col text-right  pt-16" id="about-me-id">
-        <h1 className="text-7xl  font-montserrat font-bold">ABOUT ME</h1>
-        <div className="flex flex-row gap-10 pt-16">
-          <Image
-            className="hero-image rounded-lg"
-            src="/images/imagen-ale-hero.jpg"
-            alt="Hero home image"
-            width={450}
-            height={1}
-          />
-          <div className="flex flex-col font-light">
-            <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
-              My name is Alejandro Otero, I was born in Argentina and I am
-              fourty five years old. I have been working as Front End and Web
-              developer for the last six years. My experience also include six
-              years as Java backend developer. I have studied the carrer of
-              Multiplatform Application Development on IES Fernando Wirtz Suarez
-              of La Coruña, Spain.
-            </p>
-            <br></br>
-            <br></br>
-            <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
-              Since 2018, I have specialized in Front End Development and
-              Designing, with a particular focus on React and Next.js, which I
-              use extensively to craft responsive, visually appealing designs.
-              Additionally, I work with a wide range of technologies, including
-              HTML5 (I focus on the use of Flexbox Model), CSS3 (Tailwind
-              framework and SASS processor included), JavaScript and TypeScript,
-              Vue and Angular, jQuery, BootStrap and REST and SOAP Consuption,
-              among other technologies. Also, I have 6 years of experience with
-              different CMSs, mainly with Wordpress, but also with Shopify,
-              Webflow, Wix, Drupal and Joomla. I also would like to mention that
-              I have nine years of experience with GIT and GIT repositories like
-              GitHub and BitBucket. Currently, I am learning React Native, which
-              I hope to master soon.
-            </p>
-            <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
-              The main tools I use for design are Figma, XD, Photoshop and
-              Illustrator with a particular focus on Figma.
-            </p>
-            <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
-              Regarding to the methodologies I have worked with, I have nine
-              years of experience with Agile, mainly with Scrum, but also with
-              Kanban and XP, among others. For project task management I have
-              been using Jira all my carreer, but also Asana, Coda and Trello.
-            </p>
-            <p className="text-lg" style={{ lineHeight: "1.300" }}>
-              Beyond my technical proficiency, I am passionate about blending
-              clean code with visually impactful designs to build solutions that
-              are highly functional, user-friendly and full responsive. I pride
-              myself on maintaining clear communication and consistently
-              delivering high-quality work on time.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col h-full items-center pt-10">
-          <UpDownButtonToSkills />
-        </div>
-        <div className="flex flex-col pt-16" id="skills-id">
-          <h1 className="text-7xl  font-montserrat font-bold pb-14">SKILLS</h1>
-          <div className="flex flex-row w-full">
-            <div className=" w-full">
-              <ProgrammingSkillsChart />
-            </div>
-            <div className=" w-full">
-              <DesignSkillsChart />
+    <div className="flex flex-col items-center pb-8 w-full">
+      <div className="flex flex-col items-center pb-8 w-1490">
+        <HeroHome />
+        <div className="flex flex-col text-right  pt-16" id="about-me-id">
+          <h1 className="text-7xl  font-montserrat font-bold">ABOUT ME</h1>
+          <div className="flex flex-row gap-10 pt-16">
+            <Image
+              className="hero-image rounded-lg"
+              src="/images/imagen-ale-hero.jpg"
+              alt="Hero home image"
+              width={450}
+              height={1}
+            />
+            <div className="flex flex-col font-light">
+              <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
+                My name is Alejandro Otero, I was born in Argentina and I am
+                fourty five years old. I have been working as Front End and Web
+                developer for the last six years. My experience also include six
+                years as Java backend developer. I have studied the carrer of
+                Multiplatform Application Development on IES Fernando Wirtz
+                Suarez of La Coruña, Spain.
+              </p>
+              <br></br>
+              <br></br>
+              <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
+                Since 2018, I have specialized in Front End Development and
+                Designing, with a particular focus on React and Next.js, which I
+                use extensively to craft responsive, visually appealing designs.
+                Additionally, I work with a wide range of technologies,
+                including HTML5 (I focus on the use of Flexbox Model), CSS3
+                (Tailwind framework and SASS processor included), JavaScript and
+                TypeScript, Vue and Angular, jQuery, BootStrap and REST and SOAP
+                Consuption, among other technologies. Also, I have 6 years of
+                experience with different CMSs, mainly with Wordpress, but also
+                with Shopify, Webflow, Wix, Drupal and Joomla. I also would like
+                to mention that I have nine years of experience with GIT and GIT
+                repositories like GitHub and BitBucket. Currently, I am learning
+                React Native, which I hope to master soon.
+              </p>
+              <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
+                The main tools I use for design are Figma, XD, Photoshop and
+                Illustrator with a particular focus on Figma.
+              </p>
+              <p className="text-lg pb-4" style={{ lineHeight: "1.300" }}>
+                Regarding to the methodologies I have worked with, I have nine
+                years of experience with Agile, mainly with Scrum, but also with
+                Kanban and XP, among others. For project task management I have
+                been using Jira all my carreer, but also Asana, Coda and Trello.
+              </p>
+              <p className="text-lg" style={{ lineHeight: "1.300" }}>
+                Beyond my technical proficiency, I am passionate about blending
+                clean code with visually impactful designs to build solutions
+                that are highly functional, user-friendly and full responsive. I
+                pride myself on maintaining clear communication and consistently
+                delivering high-quality work on time.
+              </p>
             </div>
           </div>
           <div className="flex flex-col h-full items-center pt-10">
-            <UpDownButtonToLastSkills />
+            <UpDownButtonToSkills />
           </div>
-          <div className="flex flex-row w-full pt-20 pb-4" id="last-skills-id">
-            <div className=" w-full">
-              <ProgrammingSkillsChart />
+          <div className="flex flex-col pt-16" id="skills-id">
+            <h1 className="text-7xl  font-montserrat font-bold pb-14">
+              SKILLS
+            </h1>
+            <div className="flex flex-row w-full">
+              <div className=" w-full">
+                <ProgrammingSkillsChart />
+              </div>
+              <div className=" w-full">
+                <DesignSkillsChart />
+              </div>
             </div>
-            <div className=" w-full">
-              <DesignSkillsChart />
+            <div className="flex flex-col h-full items-center pt-14">
+              <UpDownButtonToLastSkills />
+            </div>
+            <div
+              className="flex flex-row w-full pt-20 pb-4"
+              id="last-skills-id"
+            >
+              <div className=" w-full">
+                <ProgrammingSkillsChart />
+              </div>
+              <div className=" w-full">
+                <DesignSkillsChart />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full items-end pt-24 pr-10">
+      <div className="flex flex-col w-full items-end pt-4 pr-10">
         <UpDownButtonToTop />
       </div>
     </div>
