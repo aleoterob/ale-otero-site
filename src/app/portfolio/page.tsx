a46b66d917e84d734e4b2bf519d6b7db46c7ca5c
Alejandro Otero <aleoterob@gmail.com>
2024-12-31 13:03:01 -0300

    Adds portfolio page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio | Ale Otero Site",
+  description: "Alejandro Otero Portfolio Site",
+};
+
+export default function Portfolio() {
+  return <div className=""></div>;
+}
