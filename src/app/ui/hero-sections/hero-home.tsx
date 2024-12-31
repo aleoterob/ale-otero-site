017055b7b03fac9c8680605c7fbd3a53769c5eff
Alejandro Otero <aleoterob@gmail.com>
2024-12-31 11:27:33 -0300

    Updates hero home animation

--- src/app/ui/hero-sections/hero-home.tsx
+++ src/app/ui/hero-sections/hero-home.tsx
@@ -8,44 +8,50 @@ import PortfolioButton from "../buttons/portfolio-button";
 
 const HeroHome = () => {
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const heroImage = document.querySelector(".hero-image");
-      const my = document.querySelector(".my");
-      const personal = document.querySelector(".personal");
-      const portfolio = document.querySelector(".portfolio");
-      const site = document.querySelector(".site");
+    const timeline = gsap.timeline();
 
-      console.log("heroImage:", heroImage);
-      console.log("my:", my);
-      console.log("personal:", personal);
-      console.log("portfolio:", portfolio);
-      console.log("site:", site);
-
-      const tl = gsap.timeline({
-        defaults: { duration: 1, ease: "power2.out" },
-      });
-
-      // Configuración inicial
-      gsap.set(heroImage, { xPercent: -101, autoAlpha: 0 });
-      gsap.set(my, { xPercent: -101, autoAlpha: 0 });
-      gsap.set(personal, { xPercent: 101, autoAlpha: 0 });
-      gsap.set(portfolio, { xPercent: 101, autoAlpha: 0 });
-      gsap.set(site, { xPercent: 101, autoAlpha: 0 });
-
-      // Animaciones
-      tl.to(heroImage, { xPercent: 0, autoAlpha: 1 })
-        .to(my, { xPercent: 0, autoAlpha: 1 })
-        .to(personal, { xPercent: 0, autoAlpha: 1 }, "-=0.5")
-        .to(portfolio, { xPercent: 0, autoAlpha: 1 }, "-=0.3")
-        .to(site, { xPercent: 0, autoAlpha: 1 }, "-=0.3");
-    } else {
-      console.error("One or more elements are not found in the DOM.");
-    }
+    timeline
+      .from(".my", {
+        x: "-100%", // Mueve desde fuera de la pantalla a la izquierda
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+      })
+      .from(
+        ".personal",
+        {
+          x: "100%", // Mueve desde fuera de la pantalla a la derecha
+          opacity: 0,
+          duration: 1,
+          ease: "power2.out",
+        },
+        "-=0.5" // Superpone la animación con 0.5 segundos de retraso
+      )
+      .from(
+        ".portfolio",
+        {
+          x: "-100%", // Mueve desde fuera de la pantalla a la izquierda
+          opacity: 0,
+          duration: 1,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        ".site",
+        {
+          y: "100%", // Mueve desde fuera de la pantalla por abajo
+          opacity: 0,
+          duration: 1,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center  w-full hero-home-container -mt-75">
-      <div className="flex flex-col gap-10 flex-wrap  justify-start items-center  h-screen w-full">
+    <div className="flex flex-col justify-center items-center w-full hero-home-container -mt-75">
+      <div className="flex flex-col gap-10 flex-wrap justify-start items-center h-screen w-full">
         <div className="flex flex-row justify-end items-end w-full pt-24 z-10">
           <SocialButtons />
         </div>
@@ -53,15 +59,15 @@ const HeroHome = () => {
           autoPlay
           loop
           muted
-          className="absolute left-0 w-full object-cover  h-screen"
+          className="absolute left-0 w-full object-cover h-screen"
         >
           <source src="/videos/uhd_30fps.mp4" type="video/mp4" />
         </video>
         <div
-          className="flex flex-row  items-start justify-start text-8xl font-montserrat font-bold pt-16 w-full"
+          className="flex flex-row items-start justify-start text-8xl font-montserrat font-bold pt-16 w-full"
           style={{ lineHeight: "0.90" }}
         >
-          <div className="flex flex-col justify-start items-start w-full pl-48">
+          <div className="flex flex-col justify-start items-start w-full pl-60">
             <p className="my">MY</p>
             <p className="personal">PERSONAL</p>
             <p className="portfolio">PORTFOLIO</p>
